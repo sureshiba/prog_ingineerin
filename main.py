0f6b1d1505cb78a63393afc73fccad79fb975432
sureshiba <149683559+sureshiba@users.noreply.github.com>
2023-12-12 02:41:04 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ pipe = pipeline("text-classification", model="ProsusAI/finbert")
 
 # Определение основного контента приложения
 def main():
-    # Заголовок
+    
     st.title("Классификация текста с использованием finbert")
 
     # Поле для ввода текста
     input_text = st.text_input("Введите ваш текст:")
 
     # Обработка ввода и вывод результата
-    if st.button("Классифицировать"):
+    if st.button("OK"):
         result = pipe(input_text)
         st.write(result)
 
-# Запуск основного контента
+
 if __name__ == "__main__":
     main()
