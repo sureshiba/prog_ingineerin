336a7ceca9c73161dd48cef099878e09aacea4a7
sureshiba <149683559+sureshiba@users.noreply.github.com>
2023-12-12 14:10:41 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import streamlit as st
 from transformers import pipeline
 
-# Создаем конвейер (pipeline) для классификации текста с использованием модели finbert
+
 pipe = pipeline("text-classification", model="ProsusAI/finbert")
 
-# Определение основного контента приложения
+
 def main():
-    
-    st.title("Классификация текста с использованием finbert")
+    # Заголовок
+    st.title("классификация текста ")
 
     # Поле для ввода текста
     input_text = st.text_input("Введите ваш текст:")
 
     # Обработка ввода и вывод результата
-    if st.button("OK"):
+    if st.button("классифицировать"):
         result = pipe(input_text)
         st.write(result)
 
